9a1cccc296d7a54187bae061e391c5f23805e2d3
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2017-12-31 14:38:06 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
    masih testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from "angularfire2/database";
+import { FirebaseListObservable} from "angularfire2/database-deprecated";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    dataFirebase: any;
+    dataDariPython;
+    test;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+              public db: AngularFireDatabase) {
+        // this.dataFirebase = this.db.list('/user').valueChanges().subscribe(data => {
+        //     this.dataDariPython = data;
+        //     // console.log(data)
+        // });
+      this.test = [1, 7];
   }
 
+  public tampil() {
+      alert(this.test[1]);
+  }
 }
