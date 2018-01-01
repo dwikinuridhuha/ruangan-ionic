d9a0c0c5c7d96a4c40ba53b4f32fa9f2762fec5b
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2018-01-01 22:48:00 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com> memasukan ke koneksi laravel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule} from 'angularfire2/database';
-
-var config = {
-    apiKey: "AIzaSyDjz1y2ccW19R4_bEBnPkO4uaMlZHChkQw",
-    authDomain: "count-4dd03.firebaseapp.com",
-    databaseURL: "https://count-4dd03.firebaseio.com",
-    projectId: "count-4dd03",
-    storageBucket: "count-4dd03.appspot.com",
-    messagingSenderId: "456357071165"
-};
-
-
 @NgModule({
   declarations: [
     MyApp,
@@ -27,9 +14,7 @@ var config = {
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-      AngularFireModule.initializeApp(config),
-      AngularFireDatabaseModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,30 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { AngularFireDatabase } from "angularfire2/database";
-import { FirebaseListObservable} from "angularfire2/database-deprecated";
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-    dataFirebase: any;
-    dataDariPython;
-    test;
 
-    http = http.get("http://localhost:8000/");
+  constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController,
-              public db: AngularFireDatabase) {
-        // this.dataFirebase = this.db.list('/user').valueChanges().subscribe(data => {
-        //     this.dataDariPython = data;
-        //     // console.log(data)
-        // });
-      this.test = [1, 7];
   }
 
-  public tampil() {
-      alert(this.test[1]);
-  }
 }
