b9c988f1012085435077331b7f71e69ea5947419
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2018-01-03 08:17:24 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com> masih gj

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Http } from "@angular/http";
+
 import 'rxjs/add/operator/map';
 import {Server} from "../../provider/server";
 import {DataSource} from "../../provider/datasource";
+
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'page-home',
@@ -12,7 +14,10 @@ import {DataSource} from "../../provider/datasource";
 })
 export class HomePage {
 
-  items: any;
+    @ViewChild('barCanvas') barCanvas;
+
+    items;
+    barChart: any;
 
   constructor(public navCtrl: NavController,
               public db: Server,
@@ -21,5 +26,42 @@ export class HomePage {
       this.dataSource.source = data.json();
     });
   }
+
+  tampil() {
+      alert(this.dataSource.source[0].masuk);
+  }
+
+    ionViewDidLoad() {
+      this.barChart = new Chart(this.barCanvas.nativeElement, {
+          data: {
+              labels: ["Red", "Blue"],
+              datasets: [{
+                  label: '# of Votes',
+                  data: [
+                      5,
+                      2
+                  ],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+      });
+    }
 
 }
